3a9c94e182a12e4eef32893b102d8c9e2f89260b
xing <y1486170457@qq.com>
2021-07-16 16:55:24 +0800

    watch registry event

--- operator/hooks/bandwidth-limit-hook.sh
+++ operator/hooks/bandwidth-limit-hook.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-if [[ $1 == "--config" ]];then
-  cat << EOF
+if [[ $1 == "--config" ]] ; then
+  cat <<EOF
 configVersion: v1
 kubernetes:
 - apiVersion: v1
   kind: Pod
-  executeHookOnEvent:
-  - Added
+  executeHookOnEvent: ["Added", "Deleted","Modified"]
+  labelSelector:
+    matchLabels:
+      app: registry
+  namespace:
+    nameSelector:
+      matchNames: ["default"]
 EOF
 else
-  podName=$(jq -r .[0].object.metadata.name $BINDING_CONTEXT_PATH)
-  echo "Pod '${podName}' added"
+  type=$(jq -r '.[0].type' $BINDING_CONTEXT_PATH)
+  if [[ $type == "Event" ]] ; then
+    podName=$(jq -r '.[0].object.metadata.name' $BINDING_CONTEXT_PATH)
+    echo "Pod '${podName}' added"
+    rates=$(jq -r '.[0].object.metadata.annotations.egress-bandwidth' $BINDING_CONTEXT_PATH)
+    port=$(jq -r '.[0].object.spec.containers.')
+    container=$()
+    hostnetwork=()
+    #set qdisc rules for host
+  fi
 fi
-
- 
